93ec31b49914de70702b3d3fa7ac3d36861a388b
M-Saleh-N <salehnasser65784@gmail.com>
2025-01-27 14:26:22 +0300

    added the answer

--- exercise1.js
+++ exercise1.js
@@ -2,4 +2,10 @@ const car = {
     brand : 'Volvo',
     model : 'EX90'
   };
-  
+
+  //There are two correct syntax to alert Volvo, name them
+
+  //1. dotted notation
+console.log(car.brand);
+
+
